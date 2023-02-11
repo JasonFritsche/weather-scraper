bb8991d35b855e79e53d85a9ec9dfd9ffc6e60cb
Jason Fritsche <fritsche.jason@gmail.com>
2023-02-11 15:58:17 -0600

    switching to different source

--- scraper.js
+++ scraper.js
@@ -11,35 +11,33 @@ const pathToData = path.join(__dirname, "weatherdata.json");
 
 let data = null;
 async function scrape() {
-  const browser = await puppeteer.launch({ dumpio: true });
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   await page.goto(
-    "https://weather.com/weather/tenday/l/Austin+TX?canonicalCityId=14bfa21beb1bfe8c8d8dbc074f27e187616b7c9ecab43c6e42238e2ed1a5be47"
+    "https://www.weathertab.com/en/d/united-states/texas/austin/"
   );
 
   const weatherData = await page.evaluate(() =>
-    Array.from(
-      document.querySelectorAll(".DaypartDetails--DayPartDetail--2XOOV"),
-      (e) => ({
-        date: e.querySelector("h3").innerText,
-        highTemp: e.querySelector(".DetailsSummary--highTempValue--3PjlX")
-          .innerText,
-        lowTemp: e.querySelector(".DetailsSummary--lowTempValue--2tesQ")
-          .innerText,
-        precipitationPercentage: e.querySelector(
-          ".DetailsSummary--precip--1a98O"
-        ).innerText,
-      })
-    )
+    Array.from(document.querySelectorAll("tr.fct_day"), (e) => ({
+      date: e.querySelector("td > div.text-left > .fct-day-of-month").innerText,
+      // highTemp: e.querySelector(".weather-10-day__temperature-high").innerText,
+      // lowTemp: e.querySelector(".weather-10-day__temperature-low").innerText,
+      // precipitationPercentage: e.querySelector(".weather-10-day__precipitation")
+      //   .innerText,
+    }))
   );
 
   await browser.close();
   return weatherData;
 }
 
+const scrapedData = await scrape();
+console.log(scrapedData);
+
 // execute and persist data
-scrape().then(() => {
-  // persist data
-  fs.writeFileSync(path.resolve(pathToData), JSON.stringify(data, null, 2));
-});
+// scrape().then(() => {
+//   console.log(data);
+//   // persist data
+//   // fs.writeFileSync(path.resolve(pathToData), JSON.stringify(data, null, 2));
+// });
